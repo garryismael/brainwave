635297716c0074c4708252c103832affd71b5975
Garry <tahinjanaharygarry@gmail.com>
2024-11-14 09:00:39 +0300

    add tab-bar

--- src/app/sections/home/hero/hero.component.ts
+++ src/app/sections/home/hero/hero.component.ts
@@ -2,11 +2,14 @@ import {Component} from '@angular/core';
 import {ButtonComponent} from "@/app/components/ui/button/button.component";
 import {NgOptimizedImage} from "@angular/common";
 import {SectionComponent} from '@/app/components/section/section.component';
+import {NgIconComponent, provideIcons} from '@ng-icons/core';
+import {remixAddBoxLine, remixFileList2Line, remixHomeSmile2Line, remixSearchLine} from '@ng-icons/remixicon';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [ButtonComponent, NgOptimizedImage, SectionComponent],
+  imports: [ButtonComponent, NgOptimizedImage, SectionComponent, NgIconComponent],
+  viewProviders: [provideIcons({remixHomeSmile2Line, remixFileList2Line, remixSearchLine, remixAddBoxLine})],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
